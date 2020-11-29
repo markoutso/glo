86619e04eb1259d531169ee41bf150132163f636
komninoschat <komninoschat@gmail.com>
2020-11-29 03:49:58 +0200

    misc fixes

--- modules/ast/src/AST/AssignmentAST.ts
+++ modules/ast/src/AST/AssignmentAST.ts
@@ -3,11 +3,7 @@ import VariableAST from './VariableAST';
 import ArrayAccessAST from './ArrayAccessAST';
 import AST from './AST';
 
-export default class AssignmentAST extends BinaryAST {
-  constructor(
-    public readonly left: VariableAST | ArrayAccessAST,
-    public readonly right: AST,
-  ) {
-    super(left, right);
-  }
-}
+export default class AssignmentAST extends BinaryAST<
+  VariableAST | ArrayAccessAST,
+  AST
+> {}
